42c83393fe4a2c1a839d873f092ca8e5a5d92532
MHA <mh.almaspoor@hotmail.com>
2026-07-13 23:42:19 +0330

    feat(infrastructure): add ef core persistence and initial migration

---
+++ src/Services/ProductService/BookStore.ProductService.Infrastructure/Persistence/Migrations/20260713201017_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.ProductService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    PriceAmount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    PriceCurrency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ src/Services/ProductService/BookStore.ProductService.Infrastructure/Persistence/ProductServiceDbContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BookStore.ProductService.Infrastructure.Persistence;
+
+public sealed class ProductServiceDbContextFactory
+    :IDesignTimeDbContextFactory<ProductServiceDbContext>
+{
+    public ProductServiceDbContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<ProductServiceDbContext>();
+
+        optionBuilder.UseNpgsql("host=localhost;port=5432;Database=BookStore_ProductService;username=postgresql,password=1");
+
+        return new ProductServiceDbContext(optionBuilder.Options);
+    }
+}
